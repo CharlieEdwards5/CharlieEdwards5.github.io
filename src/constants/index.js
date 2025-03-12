8fced3541fcacd1581f3c705283e03f62938e50e
CharlieEdwards5 <147181964+CharlieEdwards5@users.noreply.github.com>
2025-03-12 13:33:59 -0700

    Deploy latest build from main

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const navLists = [
-    { id: "instagram", name: "Instagram", url: "https://www.instagram.com/wnderfuldesign/", external: true },
-    { id: "linkedin", name: "LinkedIn", url: "https://www.linkedin.com/in/charles-edwards-7540a12a7/", external: true },
-    { id: "resume", name: "Resume", url: "/resume.pdf", external: true },
-  ];
